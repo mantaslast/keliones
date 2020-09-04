db1aad6845a7e4e10828f1eee1366d70de6e4d40
r4p4la <mantas.lastakauskas6@gmail.com>
2020-09-04 20:48:36 +0300

    changes css html

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,231 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="image_wrapper big">
+            <div class="image_intro_big"></div>
+            <div class="image_intro_content">
+    
+
+            </div>
+        </div>    
+    </div>
+</div> 
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+        <div class="col-6">
+            <div class="container-title">TOP Pasiūlymai</div>
+        </div>
+        <div class="col-12">
+            <div class="top_deals">
+                <div class="top_deal">
+                    <div class="discount_badge">- 50%</div>
+                    <div class="top_deal_image"></div>
+                    <div class="top_deal_info px-3">
+                        <a href="#" class="location">Rio de Jeneiro <span class="country mx-2">(Brazil)</span></a>
+                        <div class="top_deal_icons">
+                            <i class="far fa-clock"></i>
+                            <i class="fas fa-socks"></i>
+                            <i class="fas fa-calendar-alt"></i>
                         </div>
-                    @endif
+                    </div>
+                </div>
+                <div class="top_deal">
+                    <div class="top_deal_image"></div>
+                    <div class="top_deal_info px-3">
+                        <a href="#" class="location">Rio de Jeneiro <span class="country mx-2">(Brazil)</span></a>
+                        <div class="top_deal_icons">
+                            <i class="far fa-clock"></i>
+                            <i class="fas fa-socks"></i>
+                            <i class="fas fa-calendar-alt"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="top_deal">
+                    <div class="top_deal_image"></div>
+                    <div class="top_deal_info px-3">
+                        <a href="#" class="location">Rio de Jeneiro <span class="country mx-2">(Brazil)</span></a>
+                        <div class="top_deal_icons">
+                            <i class="far fa-clock"></i>
+                            <i class="fas fa-socks"></i>
+                            <i class="fas fa-calendar-alt"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="top_deal">
+                    <div class="top_deal_image"></div>
+                    <div class="top_deal_info px-3">
+                        <a href="#" class="location">Rio de Jeneiro <span class="country mx-2">(Brazil)</span></a>
+                        <div class="top_deal_icons">
+                            <i class="far fa-clock"></i>
+                            <i class="fas fa-socks"></i>
+                            <i class="fas fa-calendar-alt"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="top_deal">
+                    <div class="top_deal_image"></div>
+                    <div class="top_deal_info px-3">
+                        <a href="#" class="location">Rio de Jeneiro <span class="country mx-2">(Brazil)</span></a>
+                        <div class="top_deal_icons">
+                            <i class="far fa-clock"></i>
+                            <i class="fas fa-socks"></i>
+                            <i class="fas fa-calendar-alt"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="top_deal">
+                    <div class="top_deal_image"></div>
+                    <div class="top_deal_info px-3">
+                        <a href="#" class="location">Rio de Jeneiro <span class="country mx-2">(Brazil)</span></a>
+                        <div class="top_deal_icons">
+                            <i class="far fa-clock"></i>
+                            <i class="fas fa-socks"></i>
+                            <i class="fas fa-calendar-alt"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
-                    {{ __('You are logged in!') }}
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-6">
+            <div class="container-title">TOP Miestai</div>
+        </div>
+        <div class="col-12">
+            <div class="top_cities">
+                <div class="top_cities_wrapper">
+                    <div class="top_city big">
+                        <div class="top_city_image"></div>
+                        <div class="info">
+                            <div class="info_title">
+                                Europe
+                            </div>
+                            <div class="info_content">
+                                pradedant nuo 2400 &euro;
+                            </div>  
+                        </div>
+                    </div>
+                </div>
+                <div class="top_cities_wrapper">
+                    <div class="top_city small">
+                        <div class="top_city_image"></div>
+                        <div class="info">
+                            <div class="info_title">
+                                Usa 
+                            </div>
+                            <div class="info_content">
+                                pradedant nuo 2400 &euro;
+                            </div>
+                        </div>
+                    </div>
+                    <div class="top_city small">
+                        <div class="top_city_image"></div>
+                        <div class="info">
+                            <div class="info_title">
+                                India 
+                            </div>
+                            <div class="info_content">
+                                pradedant nuo 2400 &euro;
+                            </div>
+                        </div>
+                    </div>
+                    <div class="top_city small">
+                        <div class="top_city_image"></div>
+                        <div class="info">
+                            <div class="info_title">
+                                Dubai 
+                            </div>
+                            <div class="info_content">
+                                pradedant nuo 2400 &euro;
+                            </div>
+                        </div>
+                    </div>
+                    <div class="top_city small">
+                        <div class="top_city_image"></div>
+                        <div class="info">
+                            <div class="info_title">
+                                London 
+                            </div>
+                            <div class="info_content">
+                                pradedant nuo 2400 &euro;
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-6">
+            <div class="container-title">Galima užsisakyti dabar</div>
+        </div>
+        <div class="col-12">
+        <div class="top_deals">
+                <div class="top_deal big">
+                    <div class="discount_badge">- 50%</div>
+                    <div class="top_deal_image"></div>
+                    <div class="top_deal_info px-3 additional">
+                        <div class="additional_wrapper">
+                            <div class="additional_info">
+                                <a href="#" class="location">Rio de Jeneiro</a>
+                                <div class="additional_info_item">
+                                    <span class="additional_info_label">City:</span>illunois,united states
+                                </div>
+                                <div class="additional_info_item">
+                                    <span class="additional_info_label">Rating:</span>1 2 3 4 5
+                                </div>
+                            </div>
+                            <div class="additional_price">
+                                420 &euro;
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="top_deal big">
+                    <div class="top_deal_image"></div>
+                    <div class="top_deal_info px-3 additional">
+                        <div class="additional_wrapper">
+                            <div class="additional_info">
+                                <a href="#" class="location">Rio de Jeneiro</a>
+                                <div class="additional_info_item">
+                                    <span class="additional_info_label">City:</span>illunois,united states
+                                </div>
+                                <div class="additional_info_item">
+                                    <span class="additional_info_label">Rating:</span>1 2 3 4 5
+                                </div>
+                            </div>
+                            <div class="additional_price">
+                                540 &euro;
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="top_deal big">
+                    <div class="top_deal_image"></div>
+                    <div class="top_deal_info px-3 additional">
+                        <div class="additional_wrapper">
+                            <div class="additional_info">
+                            <a href="#" class="location">Rio de Jeneiro</a>
+                                <div class="additional_info_item">
+                                    <span class="additional_info_label">City:</span>illunois,united states
+                                </div>
+                                <div class="additional_info_item">
+                                    <span class="additional_info_label">Rating:</span>1 2 3 4 5
+                                </div>
+                            </div>
+                            <div class="additional_price">
+                                380 &euro;
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
